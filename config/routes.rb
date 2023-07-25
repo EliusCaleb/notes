908f85e1098ea830c165194b6e4af3a14bbdf767
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-25 12:35:00 +0300

    setting up notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :notes
+  resources :users,  only: [:create]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
+  
 end
