0c4dbd912991d296c0415a18b6962b45ecab9980
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-25 12:34:50 +0300

    setting up user

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,6 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+   user = User.create(username: "alexmerced", password: "pineapple", age: 35)
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
