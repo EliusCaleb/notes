da8e59d5769132f8d54110a6ad18b53b2359be1a
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-27 02:52:49 +0300

    setting up the auth reducer not taking asyn funct

--- client/src/AppState.jsx
+++ client/src/AppState.jsx
@@ -1,31 +1,37 @@
 //////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////
-import React, { useReducer } from "react"
+import React, { useReducer } from "react";
 
-const initialState ={
-    url: '/'
-}
+const initialState = {
+  url: "http://localhost:3000/",
+  token: null,
+  username: null,
+};
 
+const reducer = (state, action) => {
+    let newState;
+  switch (action.type) {
+    case "auth":
+      newState= {...state,...action.payload}
+      return newState
+    default:
+      return state;
+  }
+};
+const AppContext = React.createContext(null);
+//////////////////////////////////////////////////////
+///APP COMPONENT
+//////////////////////////////////////////////////
+export const AppState = (props) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-const reducer = (state,action) =>{
-      switch(action.type){ 
-        default:
-            return  state
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {props.children}{" "}
+    </AppContext.Provider>
+  );
+};
 
-      }
-}
-const AppContext = React.createContext(null)
- //////////////////////////////////////////////////////
- ///APP COMPONENT
- //////////////////////////////////////////////////
-export const  AppState = (props) =>{
-
-    const [state,dispatch] = useReducer(reducer,initialState)
-
-    return <AppContext.Provider  value={{state,dispatch}}>{props.children} </AppContext.Provider>
-
-}
-
-export const useAppState = () =>{
-    return React.useContext(AppContext)
-}
+export const useAppState = () => {
+  return React.useContext(AppContext);
+};
