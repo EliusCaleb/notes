fd621362f0f572bdecb8b9fdbb22b4a2f9191dcc
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-07-27 02:52:18 +0300

    settiing up the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
-
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Auth from "./pages/Auth";
+import { Routes, Route,} from "react-router-dom";
 
 function App() {
+  
   return (
     <div className="App">
-     <h1>HelloApp</h1>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/auth/:form" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
