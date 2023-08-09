737f9c43c974084e04fa22f06a4eef5a129115cb
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-08-09 23:51:38 +0300

    edit environment for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,15 +1,15 @@
 # exit on error
-set -o errexit
+#set -o errexit
 
-bundle install
-bundle exec rails db:migrate RAILS_ENV=production
-#RAILS_ENV=production bin/rails db:migrate
-bundle install
-#bundle exec rails db:drop
-bundle exec DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
-bundle exec rails db:reset
-bundle install
-bundle exec rails db:seed
+# bundle install
+# bundle exec rails db:migrate RAILS_ENV=production
+# #RAILS_ENV=production bin/rails db:migrate
+# bundle install
+# #bundle exec rails db:drop
+# bundle exec DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
+# bundle exec rails db:reset
+# bundle install
+# bundle exec rails db:seed
 
 # bundle install
 # bundle exec rails assets:precompile
@@ -18,3 +18,28 @@ bundle exec rails db:seed
 
 #if you have seeds to run add:
 # bundle exec rails db:seed
+
+#!/bin/bash
+set -o errexit
+
+# Install gem dependencies
+bundle install
+
+# Run pending database migrations in the production environment
+bundle exec rails db:migrate RAILS_ENV=production
+
+# Bypass environment checks and reset the production database
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:reset
+
+# Seed the production database with initial data
+bundle exec rails db:seed RAILS_ENV=production
+
+# Optionally, precompile assets and clean old assets (uncomment if needed)
+# bundle exec rails assets:precompile
+# bundle exec rails assets:clean
+
+# Optionally, run additional migrations if needed (uncomment if needed)
+# bundle exec rails db:migrate RAILS_ENV=production
+
+# Optionally, run more seeds if needed (uncomment if needed)
+# bundle exec rails db:seed RAILS_ENV=production
